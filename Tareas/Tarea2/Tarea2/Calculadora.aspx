58ff778d2d49fa8b108726d73720d457c7d68c4a
MiguelRuizHdz <miguel.ruizhz@uanl.edu.mx>
2022-10-01 21:21:07 -0500

    Tarea 2: Diseño de calculadora

---
+++ Tareas/Tarea2/Tarea2/Calculadora.aspx
@@ -0,0 +1,46 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Calculadora.aspx.cs" Inherits="Tarea2.Calculadora" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <%-- Autor: Miguel Ruiz --%>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Calculadora .net" Font-Bold="True" Font-Size="52px" ForeColor="#3A0DB3" Font-Names="Ebrima"></asp:Label>
+            <br />
+            <br />
+            <asp:TextBox ID="TextBox1" Style="border-radius: 6px" runat="server" Height="110px" Width="400px"></asp:TextBox>
+            <br />
+            <asp:Button ID="btnSumar" runat="server" Text="+" BackColor="#0099FF" BorderColor="#66FFFF" BorderStyle="Outset" BorderWidth="4px" Font-Bold="True" Font-Size="70px" Height="100px" Width="100px" />
+            <asp:Button ID="btnRestar" runat="server" Text="-" BackColor="#0099FF" BorderColor="#66FFFF" BorderStyle="Outset" BorderWidth="4px" Font-Bold="True" Font-Size="70px" Height="100px" Width="100px" />
+            <asp:Button ID="btnMultiplicar" runat="server" Text="x" BackColor="#0099FF" BorderColor="#66FFFF" BorderStyle="Outset" BorderWidth="4px" Font-Bold="True" Font-Size="70px" Height="100px" Width="100px" />
+            <asp:Button ID="btnDividir" runat="server" Text="/" BackColor="#0099FF" BorderColor="#66FFFF" BorderStyle="Outset" BorderWidth="4px" Font-Bold="True" Font-Size="70px" Height="100px" Width="100px" />
+            <br />
+            <asp:Button ID="btn1" runat="server" Style="border-radius: 4px" Text="1" Font-Bold="True" Font-Size="55px" Height="100px" Width="100px" />
+            <asp:Button ID="btn2" runat="server" Style="border-radius: 4px" Text="2" Font-Bold="True" Font-Size="55px" Height="100px" Width="100px" />
+            <asp:Button ID="btn3" runat="server" Style="border-radius: 4px" Text="3" Font-Bold="True" Font-Size="55px" Height="100px" Width="100px" />
+            <asp:Button ID="btnCE" runat="server" Text="CE" BackColor="#00CC00" BorderColor="#6DFF6D" BorderStyle="Outset" BorderWidth="4px" Font-Bold="True" Font-Size="55px" Height="100px" Width="100px" />
+            <br />
+            <asp:Button ID="btn4" runat="server" Style="border-radius: 4px" Text="4" Font-Bold="True" Font-Size="55px" Height="100px" Width="100px" />
+            <asp:Button ID="btn5" runat="server" Style="border-radius: 4px" Text="5" Font-Bold="True" Font-Size="55px" Height="100px" Width="100px" />
+            <asp:Button ID="btn6" runat="server" Style="border-radius: 4px" Text="6" Font-Bold="True" Font-Size="55px" Height="100px" Width="100px" />
+            <asp:Button ID="btnDelete" runat="server" Text="<-" BackColor="#00CC00" BorderColor="#6DFF6D" BorderStyle="Outset" BorderWidth="4px" Font-Bold="True" Font-Size="55px" Height="100px" Width="100px" />
+            <br />
+            <asp:Button ID="btn7" runat="server" Style="border-radius: 4px" Text="7" Font-Bold="True" Font-Size="55px" Height="100px" Width="100px" />
+            <asp:Button ID="btn8" runat="server" Style="border-radius: 4px" Text="8" Font-Bold="True" Font-Size="55px" Height="100px" Width="100px" />
+            <asp:Button ID="btn9" runat="server" Style="border-radius: 4px" Text="9" Font-Bold="True" Font-Size="55px" Height="100px" Width="100px" />
+            <asp:ImageButton ID="btnSqrt" runat="server" Height="100px" ImageUrl="~/sqrt.png" Width="100px" />
+            <br />
+            <asp:Button ID="btn0" runat="server" Style="border-radius: 4px" Text="0" Font-Bold="True" Font-Size="55px" Height="100px" Width="100px" />
+            <asp:Button ID="btnDot" runat="server" Style="border-radius: 4px" Text="." Font-Bold="True" Font-Size="55px" Height="100px" Width="100px" />
+            <asp:Button ID="btnResultado" runat="server" Style="border-radius: 4px" Text="=" Font-Bold="True" Font-Size="55px" Height="100px" Width="200px" />
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Tareas/Tarea2/Tarea2/Calculadora.aspx.designer.cs
@@ -0,0 +1,215 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Tarea2
+{
+
+
+    public partial class Calculadora
+    {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control Label1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+
+        /// <summary>
+        /// Control TextBox1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+
+        /// <summary>
+        /// Control btnSumar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnSumar;
+
+        /// <summary>
+        /// Control btnRestar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnRestar;
+
+        /// <summary>
+        /// Control btnMultiplicar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnMultiplicar;
+
+        /// <summary>
+        /// Control btnDividir.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnDividir;
+
+        /// <summary>
+        /// Control btn1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn1;
+
+        /// <summary>
+        /// Control btn2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn2;
+
+        /// <summary>
+        /// Control btn3.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn3;
+
+        /// <summary>
+        /// Control btnCE.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnCE;
+
+        /// <summary>
+        /// Control btn4.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn4;
+
+        /// <summary>
+        /// Control btn5.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn5;
+
+        /// <summary>
+        /// Control btn6.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn6;
+
+        /// <summary>
+        /// Control btnDelete.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnDelete;
+
+        /// <summary>
+        /// Control btn7.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn7;
+
+        /// <summary>
+        /// Control btn8.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn8;
+
+        /// <summary>
+        /// Control btn9.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn9;
+
+        /// <summary>
+        /// Control ImageButton1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton ImageButton1;
+
+        /// <summary>
+        /// Control btn0.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn0;
+
+        /// <summary>
+        /// Control btnDot.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnDot;
+
+        /// <summary>
+        /// Control btnResultado.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnResultado;
+    }
+}
