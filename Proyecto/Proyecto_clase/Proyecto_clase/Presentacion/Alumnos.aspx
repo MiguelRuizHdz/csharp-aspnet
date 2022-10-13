e8216b7d13953fda333d4a4ab37f33e5b4959009
MiguelRuizHdz <miguel.ruizhz@uanl.edu.mx>
2022-10-12 21:50:04 -0500

    Tarea 3: Diseño Form Alumno

--- Proyecto/Proyecto_clase/Proyecto_clase/Presentacion/Alumnos.aspx
+++ Proyecto/Proyecto_clase/Proyecto_clase/Presentacion/Alumnos.aspx
@@ -2,4 +2,64 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <%-- Autor: Miguel Ruiz --%>
+    <h2>EDUCACION CONTINUA</h2>
+    <table>
+        <tr>
+            <td><asp:Label ID="lblMatricula" runat="server" Text="Matricula:"></asp:Label></td>
+            <td><asp:TextBox ID="txtMatricula" runat="server" Width="225px"></asp:TextBox></td>
+            <td>
+                <asp:ImageButton ID="btnBuscar" runat="server" ImageUrl="~/Imagenes/search.png" />
+            </td>
+        </tr>
+        <tr>
+            <td><asp:Label ID="lblNombre" runat="server" Text="Nombre:"></asp:Label></td>
+            <td><asp:TextBox ID="txtNombre" runat="server" Width="225px"></asp:TextBox></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td><asp:Label ID="lblDireccion" runat="server" Text="Direccion:"></asp:Label></td>
+            <td><asp:TextBox ID="txtDireccion" runat="server" Width="225px"></asp:TextBox></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td><asp:Label ID="lblGenero" runat="server" Text="Genero:"></asp:Label></td>
+            <td><asp:TextBox ID="txtGenero" runat="server" Width="225px"></asp:TextBox></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td><asp:Label ID="lblTelefono" runat="server" Text="Telefono:"></asp:Label></td>
+            <td><asp:TextBox ID="txtTelefono" runat="server" Width="225px"></asp:TextBox></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td><asp:Label ID="lblCorreo" runat="server" Text="Correo:"></asp:Label></td>
+            <td><asp:TextBox ID="txtCorreo" runat="server" Width="225px"></asp:TextBox></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td><asp:Label ID="lblCURP" runat="server" Text="CURP:"></asp:Label></td>
+            <td><asp:TextBox ID="txtCURP" runat="server" Width="225px"></asp:TextBox></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td><asp:Label ID="lblFechaNacimiento" runat="server" Text="Fecha de Nacimiento:"></asp:Label></td>
+            <td><asp:TextBox ID="txtFechaNacimiento" runat="server" Width="225px"></asp:TextBox></td>
+            <td>
+                <asp:ImageButton ID="btnCalendario" runat="server" ImageUrl="~/Imagenes/calendario.png" />
+            </td>
+        </tr><tr>
+            <td></td>
+            <td><asp:Calendar ID="calFecha" runat="server"></asp:Calendar></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>
+                <asp:ImageButton ID="imbGuardar" runat="server" ImageUrl="~/Imagenes/save.png" />
+            </td>
+        </tr>
+    </table>
+    <br />
+    <br />
+    <asp:GridView ID="grvAlumno" runat="server"></asp:GridView>
 </asp:Content>
--- Proyecto/Proyecto_clase/Proyecto_clase/Presentacion/Alumnos.aspx.designer.cs
+++ Proyecto/Proyecto_clase/Proyecto_clase/Presentacion/Alumnos.aspx.designer.cs
@@ -13,5 +13,194 @@ namespace Proyecto_clase.Presentacion
 
     public partial class WebForm1
     {
+
+        /// <summary>
+        /// Control lblMatricula.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblMatricula;
+
+        /// <summary>
+        /// Control txtMatricula.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtMatricula;
+
+        /// <summary>
+        /// Control btnBuscar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton btnBuscar;
+
+        /// <summary>
+        /// Control lblNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblNombre;
+
+        /// <summary>
+        /// Control txtNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNombre;
+
+        /// <summary>
+        /// Control lblDireccion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblDireccion;
+
+        /// <summary>
+        /// Control txtDireccion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtDireccion;
+
+        /// <summary>
+        /// Control lblGenero.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblGenero;
+
+        /// <summary>
+        /// Control txtGenero.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtGenero;
+
+        /// <summary>
+        /// Control lblTelefono.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblTelefono;
+
+        /// <summary>
+        /// Control txtTelefono.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtTelefono;
+
+        /// <summary>
+        /// Control lblCorreo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblCorreo;
+
+        /// <summary>
+        /// Control txtCorreo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCorreo;
+
+        /// <summary>
+        /// Control lblCURP.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblCURP;
+
+        /// <summary>
+        /// Control txtCURP.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCURP;
+
+        /// <summary>
+        /// Control lblFechaNacimiento.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblFechaNacimiento;
+
+        /// <summary>
+        /// Control txtFechaNacimiento.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtFechaNacimiento;
+
+        /// <summary>
+        /// Control btnCalendario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton btnCalendario;
+
+        /// <summary>
+        /// Control calFecha.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Calendar calFecha;
+
+        /// <summary>
+        /// Control imbGuardar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton imbGuardar;
+
+        /// <summary>
+        /// Control grvAlumno.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView grvAlumno;
     }
 }
